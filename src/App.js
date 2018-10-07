7709e7a36e803e8c02e3713e8e645e47da1f42c2
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-10-07 10:36:15 -0400

    Pictures are rendering
    -feat: pictures appearing on each Marker

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import MainPage from './MainPage'
 
+
 class App extends Component {
   render() {
     return (
--- src/Map.js
+++ src/Map.js
@@ -6,14 +6,40 @@ export class MapContainer extends Component {
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {},
+        markers:[],
+        APIKEY:'6286ba4b73755ef30f2e7366ae0c6fb5',
+        pictures:[]
     };
+componentDidMount() {
+    this.setState({
+            markers: [
+                {name: "Catedral de San Juan Bautista", lat: 18.4656722, lng: -66.1180264},
+                {name: "Paseo de La Princesa", lat: 18.463370, lng: -66.118666},
+                {name: "El Morro", lat: 18.4709583, lng: -66.1236127}
+            ]
+        }
+    );
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.state.APIKEY + '&tags=nyc&per_page=10&page=1&format=json&nojsoncallback=1')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (j) {
+            alert(JSON.stringify(j));
+            let picArray = j.photos.photo.map((pic) => {
 
-    onMarkerClick = (props, marker, e) =>
+                return 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
+            });
+            this.setState({pictures: picArray});
+        }.bind(this));
+}
+
+    onMarkerClick = (props, marker, e) => {
         this.setState({
             selectedPlace: props,
             activeMarker: marker,
             showingInfoWindow: true
-        });
+        })
+    };
 
     onMapClicked = (props) => {
         if (this.state.showingInfoWindow) {
@@ -23,9 +49,11 @@ export class MapContainer extends Component {
             })
         }
     };
-    render() {
-        return (
 
+
+    render() {
+        const markers = this.state.markers;
+        return (
             <Map google={this.props.google}
                  zoom={14}
                  initialCenter={{
@@ -33,20 +61,34 @@ export class MapContainer extends Component {
                  lng: -66.124498
                  }}
                  onClick={this.onMapClicked}>
-                <Marker onClick={this.onMarkerClick}
-                position={{lat: 18.463370, lng: -66.118666}}
-                name={'Paseo La Princesa'}
-                />
-                <Marker onClick={this.onMarkerClick}
-                        name={'El Morro'} />
+
+                {
+                    markers.map((marker)=>
+
+                    <Marker key={marker.name} onClick =  {this.onMarkerClick}
+                            position = { {lat:marker.lat, lng:marker.lng} }
+                            name = {marker.name}
+                    />
+                )}
 
                 <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}>
+                    marker = {this.state.activeMarker}
+                    visible = {this.state.showingInfoWindow}>
                     <div>
                         <h1>{this.state.selectedPlace.name}</h1>
+                        <div>
+                            {
+
+                                this.state.pictures.map((url)=>(
+
+                                    <img key={url} src={url}/>
+                                    )
+                                )
+                            }
+                    </div>
                     </div>
                 </InfoWindow>
+
             </Map>
         );
     }
